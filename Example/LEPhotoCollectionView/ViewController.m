4e7a7687053ae4f3bc1a474977840efa3873d827
leavez <gaojiji@gmail.com>
2017-03-28 14:16:43 +0800

    add gif demo

--- Example/LEPhotoCollectionView/ViewController.m
+++ Example/LEPhotoCollectionView/ViewController.m
@@ -5,9 +5,17 @@
 //  Created by Gao on 7/20/16.
 //  Copyright © 2016 leave. All rights reserved.
 //
+#ifdef LEPhotoCollectionView_GIF_SUPPORT
+#define SUPPORT_GIF 1
+#endif
 
 #import "ViewController.h"
+#ifdef SUPPORT_GIF
+#import <LEPhotoCollectionView_GIF/LEPhotoCollectionView.h>
+#else
 #import <LEPhotoCollectionView/LEPhotoCollectionView.h>
+#endif
+
 
 @interface ViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
 @property (nonatomic) LEGapPagingCollectionView *photoView;
@@ -30,6 +38,14 @@
     for (NSString *name in @[@"1.jpg",@"2.jpg",@"3.jpg"]) {
         [images addObject:[UIImage imageNamed:name]];
     }
+#ifdef SUPPORT_GIF
+    {
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"slow" ofType:@"gif"];
+        NSData *data = [NSData dataWithContentsOfFile:path];
+        FLAnimatedImage *image = [[FLAnimatedImage alloc] initWithAnimatedGIFData:data];
+        [images addObject:image];
+    }
+#endif
     self.data = [images copy];
 }
 
@@ -41,7 +57,18 @@
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
     LEDraggablePhotoCell *c = (LEDraggablePhotoCell *)cell;
-    c.innerScrollView.image = self.data[indexPath.row];
+    UIImage *image = self.data[indexPath.row];
+#ifdef SUPPORT_GIF
+    if ([image isKindOfClass:[FLAnimatedImage class]]) {
+        c.innerScrollView.imageView.animatedImage = (FLAnimatedImage *)image;
+    } else {
+#endif
+    c.innerScrollView.image = image;
+
+#ifdef SUPPORT_GIF
+    }
+#endif
+
     c.collectionView = collectionView;
     return c;
 }
